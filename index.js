fa66591c806e4215606b56d7139885c100830d6b
nizinkt <kt.nizin@gmail.com>
2021-09-08 14:14:28 +0530

    made changes in files index.html,styles.css and introduced a new index.js file.Added new navbar-feature section -team section and footer.And made website responsive

---
+++ index.js
@@ -0,0 +1,17 @@
+const menu=document.querySelector('#mobile-menu')
+const menuLinks=document.querySelector('.navbar__menu')
+
+//display mobile menu
+
+const mobileMenu=()=>{
+    menu.classList.toggle('is-active');
+    menuLinks.classList.toggle('active');
+};
+
+menu.addEventListener('click',mobileMenu);
+
+
+
+
+//disappear navbar on scroll down
+
